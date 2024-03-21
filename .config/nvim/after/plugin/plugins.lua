4d4fdac153bc431f9ed5b5fe63ed0d2cdf9028a9
reallocmb <realloc.mb@gmail.com>
2024-03-21 20:36:58 +0100

    update

--- .config/nvim/after/plugin/plugins.lua
+++ .config/nvim/after/plugin/plugins.lua
@@ -47,21 +47,6 @@ require("neodev").setup({
 -- vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end)
 
 
-local dapui = require("dapui")
-dapui.setup()
-dap.listeners.before.attach.dapui_config = function()
-    dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-    dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close()
-end
-
 -- configure harpoon
 
 local harpoon = require("harpoon")
